4798e173f83fd1cc99ea98ec42ae3b5919c80d05
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-01-18 17:23:31 -0500

    Add pause to end of uninstall batch file for Windows

--- resources/windows/uninstall.bat
+++ resources/windows/uninstall.bat
@@ -3,3 +3,4 @@ REM Delete link on Desktop
 del "%USERPROFILE%\Desktop\Watir Robot.lnk"
 REM Rename the wr-gems.jar file back to original
 ren lib\ruby\wr-gems-unpacked.jar lib\ruby\wr-gems.jar
+pause
