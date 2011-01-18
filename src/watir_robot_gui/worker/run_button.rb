4fd9a7eb1904fd0b1b2aec25b02c4e35b7511a65
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-01-17 20:22:58 -0500

    Make sure output directory is a directory

--- src/watir_robot_gui/worker/run_button.rb
+++ src/watir_robot_gui/worker/run_button.rb
@@ -10,11 +10,18 @@ module WatirRobotGui
         self.button.text = "Tests Running"
         self.button.set_enabled(false)
 
+        # Ensure the parameter to -d below is a directory
+        if File.directory? self.test_path
+          output_path = self.test_path
+        else
+          output_path = File.dirname(self.test_path)
+        end
+
         # Don't know if/how we can start RF tests via a class,
         # so for now we're just running it command-line style
         rf_jar = 'lib/standalone/robotframework.jar'
         # @TODO get some kind of logging in place
-        results = IO.popen("java -jar #{rf_jar} -T -d #{self.test_path} #{self.test_path}")
+        results = IO.popen("java -jar #{rf_jar} -T -d #{output_path} #{self.test_path}")
       end
 
       def done
