45ab173af25ac1f137028f2c03e06e72124b3b6b
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-01-18 12:00:32 -0500

    Flesh out uninstall batch file for Windows

--- resources/windows/uninstall.bat
+++ resources/windows/uninstall.bat
@@ -1,3 +1,5 @@
 @echo off
 REM Delete link on Desktop
-del "%USERPROFILE%\Desktop\Watir Robot.lnk
+del "%USERPROFILE%\Desktop\Watir Robot.lnk"
+REM Rename the wr-gems.jar file back to original
+ren lib\ruby\wr-gems-unpacked.jar lib\ruby\wr-gems.jar
