b053554a07e535ae26675fa2b852e31d6572842c
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-01-21 15:52:15 -0500

    Add quotation marks and take out unnecessary uninstall tasks

--- resources/windows/setup.bat
+++ resources/windows/setup.bat
@@ -2,7 +2,7 @@
 set WR_PROJECT_HOME=%CD%
 REM Create desktop shortcuts
 echo Creating Desktop Shortcuts...
-wscript %WR_PROJECT_HOME%\resources\create_shortcuts.vbs
+wscript "%WR_PROJECT_HOME%\resources\create_shortcuts.vbs"
 echo Shortcut created successfully!
 echo Installation complete! See the README for more instructions.
 pause
--- resources/windows/uninstall.bat
+++ resources/windows/uninstall.bat
@@ -2,8 +2,5 @@
 REM Delete link on Desktop
 echo Deleting Desktop shortcut...
 del "%USERPROFILE%\Desktop\Watir Robot.lnk"
-REM Rename the wr-gems.jar file back to original
-echo Renaming files affected during installation...
-cd lib\ruby
-ren wr-gems-unpacked.jar wr-gems.jar
+echo Uninstall complete.
 pause
